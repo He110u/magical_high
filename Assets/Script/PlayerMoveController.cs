89b00865a4e0111e7960af3a9f26eb20597a5861
He110u <jisue423@gmail.com>
2026-03-23 12:07:05 +0900

    03_23(move, camera)
    이동, 카메라 구현

---
+++ Assets/Script/PlayerMoveController.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform cameraTransform;
+
+    [Header("Movement")]
+    [SerializeField] private float maxSpeed = 6f;
+    [SerializeField] private float acceleration = 20f;
+    [SerializeField] private float deceleration = 25f;
+
+    private Vector2 input;
+    private Vector3 currentVelocity;
+
+    public void OnMove(InputValue value)
+    {
+        input = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
+
+        camForward.y = 0f;
+        camRight.y = 0f;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 moveDir = camForward * input.y + camRight * input.x;
+
+        Vector3 targetVelocity = moveDir * maxSpeed;
+
+        float accelRate = (moveDir.magnitude > 0.01f) ? acceleration : deceleration;
+
+        currentVelocity = Vector3.MoveTowards(
+            currentVelocity,
+            targetVelocity,
+            accelRate * Time.fixedDeltaTime
+        );
+
+        rb.linearVelocity = new Vector3(currentVelocity.x, rb.linearVelocity.y, currentVelocity.z);
+
+    }
+}
